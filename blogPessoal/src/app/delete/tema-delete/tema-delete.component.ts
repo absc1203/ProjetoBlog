0234b6a9b0fb9fb329701de715a0b79d0288d39c
absc1203 <absc1203@gmail.com>
2021-02-26 00:12:54 -0300

    crud completo tema

--- blogPessoal/src/app/delete/tema-delete/tema-delete.component.ts
+++ blogPessoal/src/app/delete/tema-delete/tema-delete.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Tema } from 'src/app/model/Tema';
 import { TemaService } from 'src/app/service/tema.service';
+import { environment } from 'src/environments/environment.prod';
 
 @Component({
   selector: 'app-tema-delete',
@@ -11,6 +12,7 @@ import { TemaService } from 'src/app/service/tema.service';
 export class TemaDeleteComponent implements OnInit {
 
   tema: Tema = new Tema()
+  idTema: number
 
   constructor(
     private temaService: TemaService,
@@ -19,7 +21,23 @@ export class TemaDeleteComponent implements OnInit {
 
   ) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+
+    }
+
+    this.idTema = this.route.snapshot.params['id']
+    this.findByIdTema(this.idTema)
+  }
+
+  findByIdTema(id: number){
+    this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema)=> {this.tema = resp})
+  }
+
+  apagar(){
+    this.temaService.deleteTema(this.idTema).subscribe(()=> {alert('Tema apagado com sucesso')
+    this.router.navigate(['/tema'])})
   }
 
 }
